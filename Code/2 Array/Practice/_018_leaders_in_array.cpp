f500a105078980a927f7b2708b2c6ee43892ffa5
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2024-08-13 08:47:47 +0530

    Add new code files and fix bugs in existing code

---
+++ Code/2 Array/Practice/_018_leaders_in_array.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int leader(int arr[],int n){
+    
+}
+
+int main(){
+    int arr[] = {7, 10, 4, 3, 6, 5, 2}, n = 7;
+    cout<<"Leasers in Reverse order";
+    leader(arr,n);
+    cout<<endl;
+}
+
