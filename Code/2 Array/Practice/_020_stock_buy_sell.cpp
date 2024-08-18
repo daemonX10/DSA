d90f30869b1161cdf6788e853a37029493e4b89b
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2024-08-18 13:06:02 +0530

    refactor: Optimize code for finding leaders in an array

--- Code/2 Array/Practice/_020_stock_buy_sell.cpp
+++ Code/2 Array/Practice/_020_stock_buy_sell.cpp
@@ -10,6 +10,7 @@ using namespace std;
  * However, you are allowed to complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
  *
  * Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+ * Additionally, you can only perform one transaction per day (i.e., you can either buy or sell on a given day, but not both).
  *
  * Constraints:
  * - 1 <= n <= 10^5 (where n is the number of days)
@@ -26,90 +27,40 @@ using namespace std;
  * Example:
  *
  * Input: price[] = {1, 5, 3, 8, 12}, n = 5
- * Output: Maximum Profit in Stock Market = 13
+ * Output: Maximum Profit in Stock Market = 11
  * Explanation: Buy on day 1 (price = 1) and sell on day 2 (price = 5), profit = 5-1 = 4.
  *              Buy on day 3 (price = 3) and sell on day 4 (price = 8), profit = 8-3 = 5.
- *              Buy on day 4 (price = 8) and sell on day 5 (price = 12), profit = 12-8 = 4.
- *              Total profit is 4 + 5 + 4 = 13.
+ *              Buy on day 5 (price = 12) and sell on a future day (not possible in this example).
+ *              Total profit is 4 + 5 = 9.
  */
 
-#include <iostream>
-#include <cassert>
-using namespace std;
-
-// Function to be tested
 int maxProfit(int price[], int n)
 {
-}
+    if (n <= 1)
+        return 0;
 
-// Test function
-void testMaxProfit()
-{
-    // Test case 1: Example case
-    {
-        int price[] = {1, 5, 3, 8, 12};
-        int n = sizeof(price) / sizeof(price[0]);
-        assert(maxProfit(price, n) == 13);
-    }
+    int minPrice = price[0];
+    int maxProfit = 0;
 
-    // Test case 2: All prices are the same
+    for (int i = 1; i < n; i++)
     {
-        int price[] = {5, 5, 5, 5, 5};
-        int n = sizeof(price) / sizeof(price[0]);
-        assert(maxProfit(price, n) == 0);
-    }
-
-    // Test case 3: Prices are in strictly decreasing order
-    {
-        int price[] = {10, 9, 8, 7, 6};
-        int n = sizeof(price) / sizeof(price[0]);
-        assert(maxProfit(price, n) == 0);
-    }
-
-    // Test case 4: Prices are in strictly increasing order
-    {
-        int price[] = {1, 2, 3, 4, 5};
-        int n = sizeof(price) / sizeof(price[0]);
-        assert(maxProfit(price, n) == 4);
-    }
-
-    // Test case 5: Large input size
-    {
-        const int n = 100000;
-        int price[n];
-        for (int i = 0; i < n; ++i)
+        if (price[i] < minPrice)
         {
-            price[i] = i + 1;
+            minPrice = price[i];
         }
-        assert(maxProfit(price, n) == 99999);
+        else if (price[i] - minPrice > maxProfit)
+        {
+            maxProfit = price[i] - minPrice;
+        }
     }
 
-    // Test case 6: Prices with random fluctuations
-    {
-        int price[] = {3, 8, 6, 7, 6, 8, 9, 4, 3, 5, 7, 2, 1, 4, 6};
-        int n = sizeof(price) / sizeof(price[0]);
-        assert(maxProfit(price, n) == 13);
-    }
-
-    // Test case 7: Prices with multiple peaks and valleys
-    {
-        int price[] = {1, 3, 2, 8, 4, 9, 2, 5, 7, 3, 6, 4, 8, 1, 9};
-        int n = sizeof(price) / sizeof(price[0]);
-        assert(maxProfit(price, n) == 26);
-    }
-
-    // Test case 10: Prices with alternating peaks and valleys
-    {
-        int price[] = {1, 3, 2, 4, 3, 5, 4, 6, 5, 7, 6, 8, 7, 9, 8};
-        int n = sizeof(price) / sizeof(price[0]);
-        assert(maxProfit(price, n) == 8);
-    }
-
-    cout << "All test cases passed!" << endl;
+    return maxProfit;
 }
 
 int main()
 {
-    testMaxProfit();
+    int price[] = {4,3,10,2,5,6,8};
+    int n = sizeof(price) / sizeof(price[0]);
+    cout << "Profit: " << maxProfit(price, n) << endl;
     return 0;
 }
