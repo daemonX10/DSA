62a07edb1731fde6559d5b1abe14f80a9282d474
Damodar Yadav <115637298+MrLEGENDx10@users.noreply.github.com>
2024-08-18 13:06:46 +0530

    refactor: Optimize code for trapping rainwater in an array

--- Code/2 Array/Practice/_021_trap_rain.cpp
+++ Code/2 Array/Practice/_021_trap_rain.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 
 int trap_rain(int heights[],int n){
-    int total_water;
-    for(int i=0;i<n-1;i++){
-        if(heights[i] > heights[i+1] && heights[i+1] < heights[i+2]){
-            if(heights[i+2]>heights[i]){
-                total_water+=heights[i+2]-heights[i+1];
-            }else{
-                total_water+=heights[i]-heights[i+1];
-            }
+    int traped_water=0;
+    int left_max[n],right_max[n];
+    left_max[0]=heights[0];
+    right_max[n-1]=heights[n-1];
+    for(int i=1;i<n-1;i++){
+        left_max[i]=max(left_max[i-1],heights[i]);
+        right_max[n-1-i]=max(heights[n-1-i],right_max[n-i]);
         }
+    for(int i=1;i<n-1;i++){
+        traped_water+=min(left_max[i],right_max[i])-heights[i];
     }
-    cout<<total_water;
-    return total_water;
+    return traped_water;
 }
 
 int main(){
     int heights[] = {4, 2, 0, 3, 2, 5};
     int n = 6;
-    cout<<"traped water"<<trap_rain(heights,n);
+    cout<<"traped water "<<trap_rain(heights,n);
 }
